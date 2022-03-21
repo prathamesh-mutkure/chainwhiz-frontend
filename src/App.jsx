6ba892f586b91fd94c2307d15a342a89587d4b37
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-03-22 01:18:29 +0530

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Articles from "./pages/Articles";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Web3</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Articles />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Articles.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Articles.scss";
+
+export default function Articles() {
+  return (
+    <div>
+      <h1>Articles</h1>
+    </div>
+  );
+}
