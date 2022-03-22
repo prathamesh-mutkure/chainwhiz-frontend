78ea91a3f75096164e4797e497f4ba165fc85fc1
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-03-22 15:39:06 +0530

    Minor Changes

--- src/components/articles/ArticleCarouselItem.jsx
+++ src/components/articles/ArticleCarouselItem.jsx
@@ -6,7 +6,7 @@ import "react-alice-carousel/lib/alice-carousel.css";
 export default function ArticleCarouselItem() {
   return (
     <div className="article-item">
-      <Paper className="img"></Paper>
+      <Paper className="img" square></Paper>
       <p>Chainwhiz - A new bounty marketplace for Web3 projects</p>
     </div>
   );
